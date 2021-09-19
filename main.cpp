285804bec46cf75ac48c99b089337075191cfc9c
Максим Едуардович <ershikov.maksim@gmail.com>
2021-09-19 19:52:29 +0300

    openGL/Glut initialization

--- main.cpp
+++ main.cpp
@@ -1,5 +1,51 @@
+#include <glut.h>
+
+
+void gameloop(int);
+void render();
+void update();
+void keyboard(int key, int a, int b);
 
 int main()
 {
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(1280, 720);
+	glutCreateWindow("Sorting algorithms visualizer");
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 1280, 0,720);
+
+	glutDisplayFunc(render);
+	glutTimerFunc(50, gameloop, 0);
+	glutSpecialFunc(keyboard);
+
+	//Game loop
+	glutMainLoop();
 	return 0;
+}
+
+void gameloop(int=0)
+{
+	render();
+	update();
+	glutTimerFunc(50, gameloop, 0);
+}
+
+void render()
+{
+	glClearColor(0.8, 0.8, 0.8, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+void update()
+{
+
+}
+
+void keyboard(int key, int a, int b)
+{
+	switch (key)
+	{
+	}
 }
